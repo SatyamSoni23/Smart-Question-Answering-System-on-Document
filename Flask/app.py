f4f42ab2ff843554e39f33c01b65309287a48a3f
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2021-08-27 23:07:37 +0530

    Add files via upload

---
+++ Flask/app.py
@@ -0,0 +1,68 @@
+from flask import *
+import pyrebase
+import os
+
+import pandas as pd
+from cdqa.utils.converters import pdf_converter
+from cdqa.pipeline import QAPipeline
+import joblib
+
+config = {
+	"apiKey": "AIzaSyDTcnYJacmlV2w4nyZPLyVw0XnQC7MEycE",
+    "authDomain": "docyqa-8a091.firebaseapp.com",
+    "projectId": "docyqa-8a091",
+	"databaseURL" : "",
+    "storageBucket": "docyqa-8a091.appspot.com",
+    "messagingSenderId": "8713193673",
+    "appId": "1:8713193673:web:7b62098c9ce6e8aa37a518",
+    "measurementId": "G-QQZHNMPNL1"
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+
+def fine_tuning_drive(question, file_name):
+  storage.child("docs/" + file_name).download("/docs/", "docs/" + file_name)
+  df = pdf_converter(directory_path="docs/")
+  pd.set_option('display.max_colwidth', -1)
+  df.head()
+  cdqa_pipeline = QAPipeline(reader='./models/bert_qa.joblib', max_df=1.0)
+  cdqa_pipeline.fit_retriever(df=df)
+  joblib.dump(cdqa_pipeline, './models/bert_qa_custom.joblib')
+  cdqa_pipeline=joblib.load('./models/bert_qa_custom.joblib')
+  prediction = cdqa_pipeline.predict(question, 1)
+  os.remove("docs/"+file_name)
+  return prediction
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		if(request.form.get('btn') == 'index'):
+		  upload = request.files['upload']
+		  path = "docs"
+		  global file_name
+		  file_name = upload.filename
+		  #print(file_name)
+		  storage.child("docs/"+file_name).put(upload)
+		  return redirect(url_for('qa'))
+		elif (request.form.get('btn') == 'qa'):
+		  print(file_name)
+		  question = request.form.get('question')
+		  answer = fine_tuning_drive(question, file_name)
+		return render_template('qa.html', answer = answer, question = question)
+	return render_template('index.html')
+  
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+    global func
+    func = request.args.get('type')
+    return render_template('upload.html')
+
+@app.route('/qa/', methods=['GET', 'POST'])
+def qa():
+    return render_template('qa.html')
+	
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
